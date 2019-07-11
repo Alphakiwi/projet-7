f81103c0c5cffb8660fe3e3c12c9f5e7b3c0ed8e
Alphakiwi <deniscossu@outlook.fr>
2019-07-11 12:25:05 +0200

    premier commit

---
+++ app/src/main/java/com/alphakiwi/projet_7/User.java
@@ -0,0 +1,37 @@
+package com.alphakiwi.projet_7;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Philippe on 30/01/2018.
+ */
+
+public class User {
+
+    private String uid;
+    private String username;
+
+    @Nullable
+    private String urlPicture;
+
+    public User() { }
+
+    public User(String uid, String username, String urlPicture) {
+        this.uid = uid;
+        this.username = username;
+        this.urlPicture = urlPicture;
+
+    }
+
+    // --- GETTERS ---
+    public String getUid() { return uid; }
+    public String getUsername() { return username; }
+    public String getUrlPicture() { return urlPicture; }
+
+
+    // --- SETTERS ---
+    public void setUsername(String username) { this.username = username; }
+    public void setUid(String uid) { this.uid = uid; }
+    public void setUrlPicture(String urlPicture) { this.urlPicture = urlPicture; }
+
+}
